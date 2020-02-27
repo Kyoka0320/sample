9f76d9680beed1e2adac55488d2b79b09f576a23
鎌田京香 <k060230320@gmail.com>
2020-02-27 11:29:27 +0800

    commentsとtasksのデータベースのサンプルを作成

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('comments')->insert([
+            'body' => '123',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+    }
+}
